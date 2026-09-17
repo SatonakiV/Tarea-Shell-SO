c40cd21052f4d833c2fe5357a096ddcc99d92ba3
midwestFaust <joaquinfelizjueves@gmail.com>
2026-09-17 19:27:59 -0300

    creados archivos de jobs, struct de jobs

---
+++ jobs.c
@@ -0,0 +1,29 @@
+#define _POSIX_C_SOURCE 200809L
+ 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+ 
+#include "jobs.h"
+ 
+typedef enum {
+    JOB_RUNNING,
+    JOB_DONE
+} JobState;
+ 
+typedef struct {
+    int id;
+    int active;                  
+    pid_t *pids;                 
+    size_t pid_count;
+    size_t pending;               
+    volatile sig_atomic_t state;  
+    int exit_code;                
+    int background;
+    int notified;                 
+    char *command_line;
+} Job;
+ 
