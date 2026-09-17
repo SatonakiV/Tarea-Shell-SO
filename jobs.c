0d1f3f9bf0932adfbabbeaf1ffea947d028327e8
midwestFaust <joaquinfelizjueves@gmail.com>
2026-09-17 19:46:10 -0300

    sigchld_handler

--- jobs.c
+++ jobs.c
@@ -34,7 +34,6 @@ static int next_job_id = 1;
 
 
 
-
 static Job *find_job_by_pid(pid_t pid) {
     for (int i = 0; i < JOBS_MAX; i++) {
         if (!jobs[i].active) {
@@ -58,3 +57,77 @@ static Job *find_job_by_id(int id) {
     return NULL;
 }
  
+
+
+
+/*                                       /;    ;\
+                                   __  \\____//
+                                  /{_\_/   `'\____
+                                  \___   (o)  (o  }
+       _____________________________/          :--'  
+   ,-,'`@@@@@@@@       @@@@@@         \_    `__\
+  ;:(  @@@@@@@@@        @@@             \___(o'o)
+  :: )  @@@@          @@@@@@        ,'@@(  `===='       
+  :: : @@@@@:          @@@@         `@@@:
+  :: \  @@@@@:       @@@@@@@)    (  '@@@'
+  ;; /\      /`,    @@@@@@@@@\   :@@@@@)
+  ::/  )    {_----------------:  :~`,~~;
+ ;;'`; :   )                  :  / `; ;
+;;;; : :   ;                  :  ;  ; :              
+`'`' / :  :                   :  :  : :
+    )_ \__;      ";"          :_ ;  \_\       `,','
+    :__\  \    * `,'*         \  \  :  \   *  8`;'*  *
+        `^'     \ :/           `^'  `-^-'   \v/ :  \/ 
+*/
+
+
+
+
+
+
+
+static void sigchld_handler(int sig) {
+    (void)sig;
+    int saved_errno = errno; //refresh
+    int status;
+    pid_t pid;
+ 
+
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        Job *job = find_job_by_pid(pid);
+ 
+        if (job == NULL) {
+            continue;
+        }
+ 
+        int code;
+        if (WIFEXITED(status)) {
+            code = WEXITSTATUS(status);
+        } else if (WIFSIGNALED(status)) {
+            code = 128 + WTERMSIG(status);
+        } else {
+            continue;
+        }
+ 
+
+
+        if (job->pending > 0) {
+            job->pending--;
+        }
+ 
+        //(cmdN de cmd1|cmd2|...|cmdN-->ultimo de salida)
+        if (pid == job->pids[job->pid_count - 1]) {
+            job->exit_code = code;
+        }
+ 
+        if (job->pending == 0) {
+            job->state = JOB_DONE; //  job listo!!
+        }
+    }
+ 
+    errno = saved_errno;
+}
+ 
+
+
+
